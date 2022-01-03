6c8736cdc1267c63bafb6200d373349938ddfee3
Ddudooo <igoa.work@gmail.com>
2022-01-03 23:03:18 +0900

    ✨스테이지 2 풀이 추가

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,9 +1,21 @@
 package tictactoe
 
+class Board(
+    val input: String
+) {
+    override fun toString(): String {
+        val stones = input.toMutableList()
+        val result = mutableListOf<String>()
+        val prePostFix = "---------"
+        for (i: Int in 0..6 step(3)) {
+            result.add(stones.subList(i,i+3).joinToString(" ", prefix = "| ", postfix = " |\n"))
+        }
+        return result.joinToString("", "$prePostFix\n", prePostFix)
+    }
+}
+
 fun main() {
-    println("""
-        X O X
-        O X O
-        X X O
-    """.trimIndent())
+    print("Enter cells: ")
+    val inputString = readLine()!!
+    println(Board(inputString))
 }
